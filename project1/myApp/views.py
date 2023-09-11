18fcb2ab2e016d4ff4aef554d6b8a3e9b744c206
Andazi <andaziboboye@gmail.com>
2023-09-11 14:32:57 +0100

    completed but need work with time validation

--- project1/myApp/views.py
+++ project1/myApp/views.py
@@ -1,21 +1,23 @@
 from django.http import JsonResponse
-#from .models import MyEndPoint
+from .models import MyEndPoint
 from django.utils import timezone
 
 
 # Create your views here.
 
 def get_data(request):
-    slack_name = request.GET.get('slack_name')
+    slack_name = request.GET.get("slack_name")
     track = request.GET.get("track")
-    
+
     current_day = timezone.now().strftime("%A")
 
-    current_utc_time = timezone.now(timezone.utc).isoformat()
+    # still dont understand the time validation yet
+    current_utc_time = timezone.now().isoformat()
+    
     github_fil_url = ""
     github_repo_url =  "https://github.com/andazi/django-with-zuri-projects"
     status_code =  200
-    
+
     context = {
         "slack_name" : slack_name,
         "current_day" : current_day,
@@ -25,7 +27,5 @@ def get_data(request):
         "github_repo_url" : github_repo_url,
         "status_code" : status_code,
     }
-    
-    return JsonResponse(context)
 
-print(timezone.now())
+    return JsonResponse(context)
--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from myApp.views import InfoView
+from myApp.views import get_data
 
 urlpatterns = [
-    path("", get_data.view, name=""),
+    path("", get_data, name="track"),
     
     path("admin/", admin.site.urls),
 ]
